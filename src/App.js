754debd0740f104c9db55cda7b8dae5897a5a1c2
Nesia <helena.maslowska17@gmail.com>
2023-09-22 12:39:31 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,35 +1,26 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import React from 'react';
 
-// to run this app on website, you need to 
-// npm run deploy
 /*
-  git add .
-  git commit -m "Configure React app for deployment to GitHub Pages"
-  git push origin master
+	To run this app on website, you need to:
+	npm run deploy
+
+	git add .
+	git commit -m "Configure React app for deployment to GitHub Pages"
+	git push origin master
+
+	master - contain source code
+	gh-pages - contain distributable version of the React app
 */
-// master - contain source code
-// gh-pages - contain distributable version of the React app
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hihi
-     	</a>
- </header>
-    </div>
-  );
+export default function App() {
+	return (
+	<div className="App">
+		<header className="App-header">
+			<button>My projects</button>
+			<button>My skills</button>
+			<button>Contact me</button>
+		</header>
+	</div>
+	);
 }
-
-export default App;
