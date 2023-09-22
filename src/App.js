b0c5472598b8821e5cf5b898b3c4e3cfbe0a9c1c
Nesia <helena.maslowska17@gmail.com>
2023-09-22 11:41:44 +0200

    save

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+// to run this app on website, you need to 
+// npm run deploy
+/*
+  git add .
+  git commit -m "Configure React app for deployment to GitHub Pages"
+  git push origin master
+*/
+// master - contain source code
+// gh-pages - contain distributable version of the React app
+
 function App() {
   return (
     <div className="App">
