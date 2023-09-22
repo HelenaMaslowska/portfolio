23e0b5463fed88dd3af7b0d80f6832089a52b082
Nesia <helena.maslowska17@gmail.com>
2023-09-22 13:49:53 +0200

    Configure React app for deployment to GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.scss';
 import React from 'react';
+import Header from './components/Header.js';
+import MainPage from './components/MainPage';
 
 /*
+													https://github.com/gitname/react-gh-pages
 	To run this app on website, you need to:
-	npm run deploy
+	npm run deploy   (on master)
 
-	git add .
+	git add .        (on master)
 	git commit -m "Configure React app for deployment to GitHub Pages"
 	git push origin master
 
-	master - contain source code
+	master - contain source code - here you send your changes and run deploy
 	gh-pages - contain distributable version of the React app
 */
 export default function App() {
 	return (
-	<div className="App">
-		<header className="App-header">
-			<button>My projects</button>
-			<button>My skills</button>
-			<button>Contact me</button>
-		</header>
+	<div>
+		<Header />
+		<MainPage />
 	</div>
 	);
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+	<header>
+		<button>My projects</button>
+		<button>My skills</button>
+		<button>Contact me</button>
+	</header>
+  )
+}
