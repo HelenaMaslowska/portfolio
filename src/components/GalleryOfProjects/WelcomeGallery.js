11ea516bd80389336613c883fcf448b05217e9b2
Nesia <helena.maslowska17@gmail.com>
2023-09-27 14:00:49 +0200

    Configure React app for deployment to GitHub Pages

--- src/components/GalleryOfProjects/WelcomeGallery.js
+++ src/components/GalleryOfProjects/WelcomeGallery.js
@@ -6,7 +6,7 @@ import './WelcomeGallery.scss'
 export default function WelcomeGallery() {
   return (
 	<section className='welcomeGallery'>
-		<div className='container_12 grid_12 content'>
+		<div className='container_12 content'>
 			<h1>Projects</h1>
 			<div>Welcome to my gallery, there is empty for now but it will change soon!</div>
 		</div>
