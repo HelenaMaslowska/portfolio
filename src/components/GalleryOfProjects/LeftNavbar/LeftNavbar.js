9f833b0357bb230ab97450278d2a0dd5fbf48a4e
Nesia <helena.maslowska17@gmail.com>
2023-10-04 23:01:51 +0200

    Configure React app for deployment to GitHub Pages

--- src/components/GalleryOfProjects/LeftNavbar/LeftNavbar.js
+++ src/components/GalleryOfProjects/LeftNavbar/LeftNavbar.js
@@ -2,13 +2,15 @@ import React from 'react'
 import './LeftNavbar.scss'
 
 import { projects } from '../../ProjectsList/Projects.js';
-
+// https://stackblitz.com/edit/react-ls1dwp?file=index.js
+// https://stackoverflow.com/questions/41887532/how-to-scroll-to-an-id-from-a-navbar
 export default function LeftNavbar() {
-  return (
-	<div className="LeftNavbar">
-	  {projects.map((topic) => ( 
-		<div className='navbarTopic'> {topic.title}</div>
-	  ))}
-	</div>
-  )
+
+	return (
+		<div className="LeftNavbar">
+		{projects.map((topic) => ( 
+			<a href={'#' + topic.title}><button className='navbarTopic'> {topic.title}</button></a>
+		))}
+		</div>
+  	)	
 }
--- src/components/GalleryOfProjects/ListOfProjects.js
+++ src/components/GalleryOfProjects/ListOfProjects.js
@@ -3,12 +3,14 @@ import './ListOfProjects.scss'
 import Project from './Project.js'
 
 export default function ListOfProjects(props) {
+
+	
   return (
 	<div id={props.topic.id}>
 		{/* SECTION TITLE */}
 		<div id={props.id} className='photoAndTilte'>
 			<img className='photo' src={props.topic.photo} alt=""/>
-			<div className='title'>{props.topic.title}</div>		
+			<div className='title'>{props.topic.title}</div>
 		</div>
 
 		{/* LIST OF PROJECTS */}
